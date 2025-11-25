bab1cdd6e034f0a78fa3cd6f4eca9c6c116085a5
Your Name <irene.germani@kcl.ac.uk>
2025-11-25 22:29:59 +0000

    Profile fix

--- components/ProfileImage.tsx
+++ components/ProfileImage.tsx
@@ -1,18 +1,17 @@
 import Image from 'next/image';
 
-const ProfileImage = (): JSX.Element => (
-  <Image
-  src="/images/me.jpeg"
-  alt="My profile image"
-  width={260}
-  height={260}
-  style={{ objectFit: "cover", objectPosition: "center" }}
-  priority
-  unoptimized
-  className="rounded-full select-none"
-/>
-
-);
+const ProfileImage = (): JSX.Element => {
+  return (
+    <div className="w-40 h-40 md:w-64 md:h-64 relative rounded-full overflow-hidden mx-auto md:mx-0">
+      <Image
+        src="/images/me.jpeg"
+        alt="My profile image"
+        fill
+        style={{ objectFit: 'cover', objectPosition: 'center' }}
+        draggable={false}
+      />
+    </div>
+  );
+};
 
 export default ProfileImage;
-
